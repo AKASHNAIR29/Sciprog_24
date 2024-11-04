807709af51a84d6244b630bcec9e91f40aaaac78
Akash Ajaykumar Nair <akash29nair@gmail.com>
2024-11-04 19:56:33 +0000

    Add Practical5 Code

--- practical5/README.md
+++ practical5/README.md
@@ -1,14 +1,44 @@
 # Practical 5
 
-This folder contains practical 5 code.
+This folder contains the information about the Practical 5
 
-## Compile:
+## Compile
 
 * gcc fib.c -o fibc
+* gcc arc.c -o arcc -lm
 
-* ./fibc
+## Execute
 
-Please input the positive integer n
-5
-The fib. series is:
-0, 1 1 2 3 5 
+*./fibc
+
+Output: 
+
+Enter the value of n: 9
+Fibonacci series up to 9:
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+*./arcc
+
+Output: 
+
+Please enter a Positive real number delta:
+0.2
+The Difference at x=-0.900000 is 0.1434035421
+The Difference at x=-0.800000 is 0.1164611240
+The Difference at x=-0.700000 is 0.0610713273
+The Difference at x=-0.600000 is 0.0305089325
+The Difference at x=-0.500000 is 0.0139075045
+The Difference at x=-0.400000 is 0.0054658390
+The Difference at x=-0.300000 is 0.0016787441
+The Difference at x=-0.200000 is 0.0003249966
+The Difference at x=-0.100000 is 0.0033422691
+The Difference at x=-0.000000 is 0.1666666667
+The Difference at x=0.100000 is 0.0033422691
+The Difference at x=0.200000 is 0.0134786150
+The Difference at x=0.300000 is 0.0016787441
+The Difference at x=0.400000 is 0.0054658390
+The Difference at x=0.500000 is 0.0139075045
+The Difference at x=0.600000 is 0.0305089325
+The Difference at x=0.700000 is 0.0610713273
+The Difference at x=0.800000 is 0.1164611240
+The Difference at x=0.900000 is 0.1434035421
--- practical5/fib.c
+++ practical5/fib.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void fib(int *a, int *b);
-
-int main(void){
-    int n;
-    printf("Please input the positive integer n\n");
-    scanf("%d", &n);
-    if (n < 1){
-        printf("n must be bigger than 1\n");
-        exit(1);
-    }
-    int f0=0;
-    int f1=1;
-    printf("The fib. series is: \n");
-    printf("%d, %d", f0, f1);
-
-    int i;
-    for (i = 2; i < n+1; i++){
-        fib(&f1, &f0);
-        printf("%d ", f1);
-        if((i+1)%10 == 0){
-            printf("\n");
-        }
-    }
-    return 0;
+// Function to compute Fibonacci series step
+void fibonacci_step(int *fn1, int *fn2) {
+    int temp = *fn1;   // Store current Fn-1
+    *fn1 = *fn1 + *fn2; // Compute Fn
+    *fn2 = temp;        // Update Fn-1 to the previous Fn-1
 }
-void fib(int *a, int *b){
-    int next;
-    next = *a + *b;
-    *b = *a;
-    *a = next;
+
+int main() {
+    int n, i;
+    
+    // Get the value of n from the user
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    
+    // Initial values for Fibonacci sequence
+    int fn1 = 1, fn2 = 0;
+    
+    // Print F0 and F1
+    printf("Fibonacci series up to %d: \n", n);
+    printf("%d, %d", fn2, fn1);
+
+    // Loop to calculate and print Fibonacci series up to n
+    for (i = 2; i <= n; i++) {
+        fibonacci_step(&fn1, &fn2);  // Update fn1 and fn2 using the function
+        printf(", %d", fn1);         // Print next Fibonacci number
+    }
+    
+    printf("\n");
+    return 0;
 }
